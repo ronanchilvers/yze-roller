5aa5f6982c164b65ea37a10ead5586643ef6b6ad
Ronan Chilvers <ronan@d3r.com>
2026-03-05 20:53:29 +0000

    Adjust key attribute die color

--- src/lib/dice-visuals.js
+++ src/lib/dice-visuals.js
@@ -2,7 +2,7 @@ import { DICE_TYPE } from "./dice.js";
 
 const DIE_COLOR = Object.freeze({
   [DICE_TYPE.ATTRIBUTE]: "#55aa5f",
-  [DICE_TYPE.KEY_ATTRIBUTE]: "#12411a",
+  [DICE_TYPE.KEY_ATTRIBUTE]: "#185922",
   [DICE_TYPE.SKILL]: "#e1c74b",
   [DICE_TYPE.STRAIN]: "#d15c5c",
   [DICE_TYPE.MODIFIER]: "#7a4cc9",
